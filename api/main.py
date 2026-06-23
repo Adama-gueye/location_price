1a7bb2491e9b4600cd2173b06c7eb97c48b23757
mamefat2004@gmail.com <adamarahma99@gmail.com>
2026-06-23 20:48:59 +0000

    second

--- api/main.py
+++ api/main.py
@@ -7,3 +7,9 @@ def read_root():
    return {
        "message": "Hello from FastAPI"
    }
+@app.get("/about")
+def about():
+   return {
+       "application": "ISI Housing",
+       "version": "1.0"
+   }
